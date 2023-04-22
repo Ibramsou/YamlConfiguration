c58910638de7e2d6e37854580bae799671c5c788
Ibramsou <kasobanboui@gmail.com>
2023-04-22 18:47:20 +0200

    Fix old package name

--- api/src/main/java/fr/bramsou/yaml/api/YamlAPI.java
+++ api/src/main/java/fr/bramsou/yaml/api/YamlAPI.java
@@ -5,7 +5,7 @@ import fr.bramsou.yaml.api.util.Reflection;
 
 public interface YamlAPI {
 
-    YamlAPI INSTANCE = Reflection.newInstance(Reflection.forName("fr.pearl.core.YamlCoreAPI").asSubclass(YamlAPI.class));
+    YamlAPI INSTANCE = Reflection.newInstance(Reflection.forName("fr.bramsou.yaml.YamlCoreAPI").asSubclass(YamlAPI.class));
 
     YamlConfigurationManager getConfigurationManager();
 }
